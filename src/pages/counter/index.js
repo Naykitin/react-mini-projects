2312c9b4234bb7ce7097038532a75eab1a9ceb66
Vladyslav <65622620+Naykitin@users.noreply.github.com>
2023-01-17 15:18:31 +0200

    update

---
+++ src/pages/counter/index.js
@@ -0,0 +1,27 @@
+import '/index.scss';
+import React from 'react';
+
+function App() {
+
+  const [count, setCount] = React.useState(0);
+
+  const onPlus = () => {
+    setCount(count + 1);
+  }
+  const onMinus = () => {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={onMinus}>- Minus</button>
+        <button className="plus" onClick={onPlus}>Plus +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
