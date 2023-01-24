5a0a3ce9d613c4a6a5e7efa1fb34dbcd852928e9
Vladyslav <65622620+Naykitin@users.noreply.github.com>
2023-01-24 17:27:39 +0200

    working on localStorage

--- src/pages/todolist/index.js
+++ src/pages/todolist/index.js
@@ -3,36 +3,74 @@ import './index.scss';
 
 function Todolist() {
 
-   const [list, setList] = React.useState(['Learn React', 'Create to-do-list']);
-   const [newList, setNewList] = React.useState();
-   const [disabled, setDisabled] = React.useState(true);
+   const [list, setList] = React.useState([
+         {
+            id: 1,
+            text: 'Learn React',
+            completed: false
+         }, 
+         {
+            id: 2,
+            text: 'Create to-do-list',
+            completed: false
+         }
+      ]);
+      
+   const [input, setInput] = React.useState('');
+   const [disabled, setDisabled] = React.useState(false);
+
+   React.useEffect(() => {
+      const data = window.localStorage.getItem('MY_NEW_LIST');
+      console.log(data);
+      if ( data !== null ) setList(JSON.parse(data));
+   }, []);
+
+   React.useEffect(() => {
+      window.localStorage.setItem('MY_NEW_LIST', JSON.stringify(list))
+   }, [list]);
 
    const onInput = (event) => {
-      setNewList([event.target.value]);
-      if (event.target.value == '') {
-         setDisabled(true);
-      } else {
-         setDisabled(false);
-      }
+      setInput(event.target.value);
+      // if (event.target.value === '') {
+      //    setDisabled(true);
+      // } else {
+      //    setDisabled(false);
+      // }
    }
 
    const addToList = () => {
-      setList(oldArray => [...oldArray, newList]);
-      setNewList('');
-      setDisabled(true);
+      setList(oldArray => [...oldArray, {
+         id: list.length + 1,
+         text: input,
+         completed: false
+      }]);
+      setInput('');
+      // setDisabled(true);
+   }
+
+   const deleteItem = (item) => {
+      setList(list.filter((obj) => obj.id !== item.id));
+   }
+
+   const onChecked = (item) => {
+      item.completed = !item.completed;
    }
 
   return (
     <div>
       <div className='todolistApp'>
          <div className='head'>
-            <input onChange={onInput} value={newList}/>
+            <input onChange={onInput} value={input}/>
             <button disabled={disabled} onClick={addToList}>Add</button>
          </div>
          <ul className='list'>
             {
-               list.map((item, index) => (
-                  <li key={index}>{item}<div></div></li>
+               list.map((item) => (
+                  <li key={item.id}>
+                     <input className='completed' type="checkbox" onClick={() => onChecked(item)} />
+                     {item.text}
+                     <div className='delete' onClick={() => deleteItem(item)}></div>
+                  </li>
                ))
             }
          </ul>
